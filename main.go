2995c0f6cfa1e9433c0617833c830d0811b94476
David Gerchikov <davi17g@gmail.com>
2019-10-09 16:36:42 +0300

    Make changes in main

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/davi17g/logging-service/records"
+	log "github.com/sirupsen/logrus"
+)
+
+//=============================================================================
+const (
+	poolSize = 5
+)
+
+//=============================================================================
+func main() {
+
+	srvAddr := flag.String("srvAddr", "localhost", "Specify service address")
+	srvPort := flag.Int("srvPort", 8080, "Specify service port")
+	dbAddr := flag.String("dbAddr", "localhost", "Specify data-base address")
+	dbPort := flag.Int("dbPort", 27017, "Specify data-base port")
+	flag.Parse()
+
+	log.Infof(
+		"Starting the service with following params: "+
+			"server-address: %s  "+
+			"server-port: %d "+
+			"database-address: %s "+
+			"database-port: %d", *srvAddr, *srvPort, *dbAddr, *dbPort)
+
+	sig := make(chan os.Signal, 1)
+	setObjectCH := make(chan interface{}, 100)
+	getRequestCH := make(chan *records.Request, 100)
+	shutdown := make(chan struct{})
+
+	dbWriter, err := getNewDataBaseWriter(*dbAddr, *dbPort)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	pool := getNewWorkerPool(getRequestCH, setObjectCH)
+	go dbWriter.writeToDB(setObjectCH, shutdown)
+
+	for i := 0; i < poolSize; i++ {
+		go pool.doWork(shutdown)
+	}
+
+	server := getNewHttpServer(*srvAddr, *srvPort, getRequestCH)
+	if err := server.start(); err != nil {
+		log.Panicf("Server was unable to start: %s", err)
+	}
+
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	close(shutdown)
+	<-sig
+	close(sig)
+}
